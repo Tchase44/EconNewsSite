b50ec60c5d2a85525774a286b4f2ac4ae354aaa2
Tchase44 <tchase4544@gmail.com>
2017-02-11 18:22:15 -0600

    basics up and running
    lots of tweeking left to do

--- js/news.js
+++ js/news.js
@@ -1,39 +1,37 @@
 
-var url = 'https://newsapi.org/v1/articles?';
-var src= 'source=';
-var sort = 'sortBy=';
-// newest
-var sortNew = 'latest';
-// current trending
-var sortPopular = 'popular';
-// original
-var sortTop = 'top';
-
-var keys = 'apiKey=15b31868ac09437dbce82f1f829c5d54'
-var ft ='financial-times';
-var te = 'the-economist';
-var fortune='fortune';
-var exUrl = url + src +fortune+ '&' +sort+sortNew+'&'+keys;
 $(document).ready(function(){
 
 
 	loadReading();
-
 });
-
 
 
 function loadReading(){
-$.getJSON('https://newsapi.org/v1/articles?source=fortune&apiKey=15b31868ac09437dbce82f1f829c5d54',function(data){
-
+		var url = 'https://newsapi.org/v1/articles?';
+	// newest
+	var sortNew = 'latest';
+	// original
+	var sortTop = 'top';
+	var keys = 'apiKey=15b31868ac09437dbce82f1f829c5d54';
+	var ft ='financial-times';
+	var te = 'the-economist';
+	var fortune='fortune';
+$.getJSON(url+'source='+ft+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img1 h6').text(data.articles[0].title);
 		$('#img1 p').text(data.articles[0].description);
+		$('#img4 h6').text(data.articles[1].title);
+		$('#img4 p').text(data.articles[1].description);
+	});
+$.getJSON(url+'source='+te+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img2 h6').text(data.articles[1].title);
 		$('#img2 p').text(data.articles[1].description);
-		$('#img3 h6').text(data.articles[2].title);
-		$('#img3 p').text(data.articles[2].description);		
-		// $('div.col-1 a').(data.articles[0].url);
-		// $('div.col-1 img').text(data.articles[0].urlToImage);
-		
-});
+		$('#img5 h6').text(data.articles[0].title);
+		$('#img5 p').text(data.articles[0].description);
+	});
+$.getJSON(url+'source='+fortune+'&sortBy='+sortTop+'&'+keys,function(data){
+		$('#img3 h6').text(data.articles[0].title);
+		$('#img3 p').text(data.articles[0].description);
+		$('#img6 h6').text(data.articles[1].title);
+		$('#img6 p').text(data.articles[1].description);
+	});
 };
