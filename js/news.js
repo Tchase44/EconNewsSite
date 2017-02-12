7964b801dda82653bfd02f2dc0db50f2717d4131
Tchase44 <tchase4544@gmail.com>
2017-02-12 12:36:13 -0600

    background-images
    load up and cover div nicely

--- js/news.js
+++ js/news.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 
 function loadReading(){
-		var url = 'https://newsapi.org/v1/articles?';
+	var url = 'https://newsapi.org/v1/articles?';
 	// newest
 	var sortNew = 'latest';
 	// original
@@ -19,19 +19,26 @@ function loadReading(){
 $.getJSON(url+'source='+ft+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img1 h6').text(data.articles[0].title);
 		$('#img1 p').text(data.articles[0].description);
+		$('#img1').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img4 h6').text(data.articles[1].title);
 		$('#img4 p').text(data.articles[1].description);
+		$('#img4').css('background-image','url('+data.articles[1].urlToImage+')');
+
 	});
 $.getJSON(url+'source='+te+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img2 h6').text(data.articles[1].title);
 		$('#img2 p').text(data.articles[1].description);
+		$('#img2').css('background-image','url('+data.articles[1].urlToImage+')');
 		$('#img5 h6').text(data.articles[0].title);
 		$('#img5 p').text(data.articles[0].description);
+		$('#img5').css('background-image','url('+data.articles[0].urlToImage+')');
 	});
 $.getJSON(url+'source='+fortune+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img3 h6').text(data.articles[0].title);
 		$('#img3 p').text(data.articles[0].description);
+		$('#img3').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img6 h6').text(data.articles[1].title);
 		$('#img6 p').text(data.articles[1].description);
+		$('#img6').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 };
