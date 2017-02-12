c6b316dcb947793a9b382beba112c3e51f148bfc
Tchase44 <tchase4544@gmail.com>
2017-02-12 16:34:40 -0600

    sources added
    added source to discription

--- js/news.js
+++ js/news.js
@@ -22,27 +22,27 @@ function loadReading(){
 	var fortune='fortune';
 $.getJSON(url+'source='+ft+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img1 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img1 p').text(data.articles[0].description);
+		$('#img1 p').text(data.articles[0].description+"(cnbc)");
 		$('#img1').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img4 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img4 p').text(data.articles[1].description);
+		$('#img4 p').text(data.articles[1].description+"(cnbc)");
 		$('#img4').css('background-image','url('+data.articles[1].urlToImage+')');
 
 	});
 $.getJSON(url+'source='+te+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img2 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img2 p').text(data.articles[0].description);
+		$('#img2 p').text(data.articles[0].description+"(the economist)");
 		$('#img2').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img5 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img5 p').text(data.articles[1].description);
+		$('#img5 p').text(data.articles[1].description+"(the economist)");
 		$('#img5').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+fortune+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img3 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img3 p').text(data.articles[0].description);
+		$('#img3 p').text(data.articles[0].description+ "(fortune)");
 		$('#img3').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img6 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img6 p').text(data.articles[1].description);
+		$('#img6 p').text(data.articles[1].description+ "(fortune)");
 		$('#img6').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 };
@@ -57,27 +57,27 @@ function loadNewest() {
 	var fortune='fortune';
 $.getJSON(url+'source='+ft+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img1 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img1 p').text(data.articles[0].description);
+		$('#img1 p').text(data.articles[0].description+"(cnbc)");
 		$('#img1').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img4 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img4 p').text(data.articles[1].description);
+		$('#img4 p').text(data.articles[1].description+"(cnbc)");
 		$('#img4').css('background-image','url('+data.articles[1].urlToImage+')');
 
 	});
 $.getJSON(url+'source='+te+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img2 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img2 p').text(data.articles[0].description);
+		$('#img2 p').text(data.articles[0].description+"(the economist)");
 		$('#img2').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img5 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img5 p').text(data.articles[1].description);
+		$('#img5 p').text(data.articles[1].description+"(the economist)");
 		$('#img5').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+fortune+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img3 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img3 p').text(data.articles[0].description);
+		$('#img3 p').text(data.articles[0].description+ "(fortune)");
 		$('#img3').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img6 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img6 p').text(data.articles[1].description);
+		$('#img6 p').text(data.articles[1].description+ "(fortune)");
 		$('#img6').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 }
