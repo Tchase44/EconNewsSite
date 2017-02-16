9713989fe9ae8b9c6a47d3b0cee8c754db2c2526
Tchase44 <tchase4544@gmail.com>
2017-02-16 16:25:55 -0600

    added more sources
    more news sources added.
    need to set a limit on title and discription length.
    maybe a trump filter

--- js/news.js
+++ js/news.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	$('#n').click(function(){
 		loadNewest();
 	})
-});
+// });
 
 
 function loadReading(){
@@ -20,30 +20,57 @@ function loadReading(){
 	var ft ='cnbc';
 	var te = 'the-economist';
 	var fortune='fortune';
+	var twsj='the-wall-street-journal';
+	var bl='bloomberg';
+	var bi='business-insider';
 $.getJSON(url+'source='+ft+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img1 a').text(data.articles[0].title).attr("href", data.articles[0].url);
 		$('#img1 p').text(data.articles[0].description+"(cnbc)");
 		$('#img1').css('background-image','url('+data.articles[0].urlToImage+')');
-		$('#img4 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img4 p').text(data.articles[1].description+"(cnbc)");
-		$('#img4').css('background-image','url('+data.articles[1].urlToImage+')');
+		$('#img12 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img12 p').text(data.articles[1].description+"(cnbc)");
+		$('#img12').css('background-image','url('+data.articles[1].urlToImage+')');
 
 	});
 $.getJSON(url+'source='+te+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img2 a').text(data.articles[0].title).attr("href", data.articles[0].url);
 		$('#img2 p').text(data.articles[0].description+"(the economist)");
 		$('#img2').css('background-image','url('+data.articles[0].urlToImage+')');
-		$('#img5 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img5 p').text(data.articles[1].description+"(the economist)");
-		$('#img5').css('background-image','url('+data.articles[1].urlToImage+')');
+		$('#img7 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img7 p').text(data.articles[1].description+"(the economist)");
+		$('#img7').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+fortune+'&sortBy='+sortTop+'&'+keys,function(data){
 		$('#img3 a').text(data.articles[0].title).attr("href", data.articles[0].url);
 		$('#img3 p').text(data.articles[0].description+ "(fortune)");
 		$('#img3').css('background-image','url('+data.articles[0].urlToImage+')');
-		$('#img6 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img6 p').text(data.articles[1].description+ "(fortune)");
-		$('#img6').css('background-image','url('+data.articles[1].urlToImage+')');
+		$('#img8 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img8 p').text(data.articles[1].description+ "(fortune)");
+		$('#img8').css('background-image','url('+data.articles[1].urlToImage+')');
+	});
+$.getJSON(url+'source='+bl+'&sortBy='+sortTop+'&'+keys,function(data){
+		$('#img4 a').text(data.articles[0].title).attr("href", data.articles[0].url);
+		$('#img4 p').text(data.articles[0].description+ "(bloomberg)");
+		$('#img4').css('background-image','url('+data.articles[0].urlToImage+')');
+		$('#img9 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img9 p').text(data.articles[1].description+ "(bloomberg)");
+		$('#img9').css('background-image','url('+data.articles[1].urlToImage+')');
+	});
+$.getJSON(url+'source='+bi+'&sortBy='+sortTop+'&'+keys,function(data){
+		$('#img5 a').text(data.articles[0].title).attr("href", data.articles[0].url);
+		$('#img5 p').text(data.articles[0].description+ "(bussiness insider)");
+		$('#img5').css('background-image','url('+data.articles[0].urlToImage+')');
+		$('#img10 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img10 p').text(data.articles[1].description+ "(bussiness insider)");
+		$('#img10').css('background-image','url('+data.articles[1].urlToImage+')');
+	});
+$.getJSON(url+'source='+twsj+'&sortBy='+sortTop+'&'+keys,function(data){
+		$('#img6 a').text(data.articles[0].title).attr("href", data.articles[0].url);
+		$('#img6 p').text(data.articles[0].description+ "(wall street journal)");
+		$('#img6').css('background-image','url('+data.articles[0].urlToImage+')');
+		$('#img11 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img11 p').text(data.articles[1].description+ "(wall street journal)");
+		$('#img11').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 };
 
@@ -52,32 +79,60 @@ function loadNewest() {
 	// newest
 	var sortNew = 'latest';
 	var keys = 'apiKey=15b31868ac09437dbce82f1f829c5d54';
-	var ft ='cnbc';
+	var bi='business-insider';
 	var te = 'the-economist';
 	var fortune='fortune';
-$.getJSON(url+'source='+ft+'&sortBy='+sortNew+'&'+keys,function(data){
+	var ind='independent';
+	var ap='associated-press';
+	var nw='newsweek';
+$.getJSON(url+'source='+bi+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img1 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img1 p').text(data.articles[0].description+"(cnbc)");
+		$('#img1 p').text(data.articles[0].description+"(bussiness insider)");
 		$('#img1').css('background-image','url('+data.articles[0].urlToImage+')');
-		$('#img4 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img4 p').text(data.articles[1].description+"(cnbc)");
-		$('#img4').css('background-image','url('+data.articles[1].urlToImage+')');
+		$('#img7 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img7 p').text(data.articles[1].description+"(bussiness insider)");
+		$('#img7').css('background-image','url('+data.articles[1].urlToImage+')');
 
 	});
 $.getJSON(url+'source='+te+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img2 a').text(data.articles[0].title).attr("href", data.articles[0].url);
 		$('#img2 p').text(data.articles[0].description+"(the economist)");
 		$('#img2').css('background-image','url('+data.articles[0].urlToImage+')');
-		$('#img5 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img5 p').text(data.articles[1].description+"(the economist)");
-		$('#img5').css('background-image','url('+data.articles[1].urlToImage+')');
+		$('#img8 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img8 p').text(data.articles[1].description+"(the economist)");
+		$('#img8').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+fortune+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img3 a').text(data.articles[0].title).attr("href", data.articles[0].url);
 		$('#img3 p').text(data.articles[0].description+ "(fortune)");
 		$('#img3').css('background-image','url('+data.articles[0].urlToImage+')');
-		$('#img6 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img6 p').text(data.articles[1].description+ "(fortune)");
-		$('#img6').css('background-image','url('+data.articles[1].urlToImage+')');
+		$('#img9 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img9 p').text(data.articles[1].description+ "(fortune)");
+		$('#img9').css('background-image','url('+data.articles[1].urlToImage+')');
+	});
+$.getJSON(url+'source='+ind+'&sortBy='+sortNew+'&'+keys,function(data){
+		$('#img4 a').text(data.articles[0].title).attr("href", data.articles[0].url);
+		$('#img4 p').text(data.articles[0].description+ "(independent)");
+		$('#img4').css('background-image','url('+data.articles[0].urlToImage+')');
+		$('#img10 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img10 p').text(data.articles[1].description+ "(independent)");
+		$('#img10').css('background-image','url('+data.articles[1].urlToImage+')');
+	});
+$.getJSON(url+'source='+ap+'&sortBy='+sortNew+'&'+keys,function(data){
+		$('#img5 a').text(data.articles[0].title).attr("href", data.articles[0].url);
+		$('#img5 p').text(data.articles[0].description+ "(associated press)");
+		$('#img5').css('background-image','url('+data.articles[0].urlToImage+')');
+		$('#img11 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img11 p').text(data.articles[1].description+ "(associated press)");
+		$('#img11').css('background-image','url('+data.articles[1].urlToImage+')');
+	});
+$.getJSON(url+'source='+nw+'&sortBy='+sortNew+'&'+keys,function(data){
+		$('#img6 a').text(data.articles[0].title).attr("href", data.articles[0].url);
+		$('#img6 p').text(data.articles[0].description+ "(newsweek)");
+		$('#img6').css('background-image','url('+data.articles[0].urlToImage+')');
+		$('#img12 a').text(data.articles[1].title).attr("href", data.articles[1].url);
+		$('#img12 p').text(data.articles[1].description+ "(newsweek)");
+		$('#img12').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 }
+});
