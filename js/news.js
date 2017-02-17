08214b8f99b2a2e0bd2faee0e096e8fe863d4e4a
Tchase44 <tchase4544@gmail.com>
2017-02-17 10:18:56 -0600

    discription edits
    limit length of discriptions as to not fill up enitre tile

--- js/news.js
+++ js/news.js
@@ -87,52 +87,55 @@ function loadNewest() {
 	var nw='newsweek';
 $.getJSON(url+'source='+bi+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img1 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img1 p').text(data.articles[0].description+"(bussiness insider)");
+		$('#img1 p').text("(bussiness insider)"+data.articles[0].description);
 		$('#img1').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img7 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img7 p').text(data.articles[1].description+"(bussiness insider)");
+		$('#img7 p').text("(bussiness insider)"+data.articles[1].description);
 		$('#img7').css('background-image','url('+data.articles[1].urlToImage+')');
 
 	});
 $.getJSON(url+'source='+te+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img2 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img2 p').text(data.articles[0].description+"(the economist)");
+		$('#img2 p').text("(the economist)"+data.articles[0].description);
 		$('#img2').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img8 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img8 p').text(data.articles[1].description+"(the economist)");
+		$('#img8 p').text("(the economist)"+data.articles[1].description);
 		$('#img8').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+fortune+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img3 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img3 p').text(data.articles[0].description+ "(fortune)");
+		$('#img3 p').text( "(fortune)"+data.articles[0].description);
 		$('#img3').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img9 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img9 p').text(data.articles[1].description+ "(fortune)");
+		$('#img9 p').text( "(fortune)"+data.articles[1].description);
 		$('#img9').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+ind+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img4 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img4 p').text(data.articles[0].description+ "(independent)");
+		$('#img4 p').text( "(independent)"+data.articles[0].description);
 		$('#img4').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img10 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img10 p').text(data.articles[1].description+ "(independent)");
+		$('#img10 p').text( "(independent)"+data.articles[1].description);
 		$('#img10').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+ap+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img5 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img5 p').text(data.articles[0].description+ "(associated press)");
+		$('#img5 p').text( "(associated press)"+data.articles[0].description);
 		$('#img5').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img11 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img11 p').text(data.articles[1].description+ "(associated press)");
+		$('#img11 p').text( "(associated press)"+data.articles[1].description);
 		$('#img11').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 $.getJSON(url+'source='+nw+'&sortBy='+sortNew+'&'+keys,function(data){
 		$('#img6 a').text(data.articles[0].title).attr("href", data.articles[0].url);
-		$('#img6 p').text(data.articles[0].description+ "(newsweek)");
+		$('#img6 p').text( "(newsweek)"+data.articles[0].description);
 		$('#img6').css('background-image','url('+data.articles[0].urlToImage+')');
 		$('#img12 a').text(data.articles[1].title).attr("href", data.articles[1].url);
-		$('#img12 p').text(data.articles[1].description+ "(newsweek)");
+		$('#img12 p').text( "(newsweek)"+data.articles[1].description);
 		$('#img12').css('background-image','url('+data.articles[1].urlToImage+')');
 	});
 }
 });
+
+// Trump filter
+
